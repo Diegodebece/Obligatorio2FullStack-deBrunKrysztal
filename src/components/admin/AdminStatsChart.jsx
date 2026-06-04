c72687018b4ed5be47b27f9f366f57fcee58d2f2
KarinaKrysztal <KK153778@fi365.ort.edu.uy>
2026-06-04 16:05:07 -0300

    Cambios en admin stats

--- src/components/admin/AdminStatsChart.jsx
+++ src/components/admin/AdminStatsChart.jsx
@@ -1,10 +1,51 @@
+import { useSelector } from "react-redux";
+import { Bar } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
 const AdminStatsChart = () => {
+  const seguimientos = useSelector(
+    (state) => state.estadisticas.seguimientos
+  );
+
+  const pendientes = seguimientos.filter(
+    (seguimiento) => seguimiento.estado === "pendiente"
+  ).length;
+
+  const viendo = seguimientos.filter(
+    (seguimiento) => seguimiento.estado === "viendo"
+  ).length;
+
+  const terminadas = seguimientos.filter(
+    (seguimiento) => seguimiento.estado === "terminada"
+  ).length;
+
+  const favoritas = seguimientos.filter(
+    (seguimiento) => seguimiento.esFavorita
+  ).length;
+
+  const data = {
+    labels: ["Pendiente", "Viendo", "Terminada", "Favoritas"],
+    datasets: [
+      {
+        label: "Cantidad de seguimientos",
+        data: [pendientes, viendo, terminadas, favoritas],
+      },
+    ],
+  };
+
   return (
     <div className="grafico">
-      <div className="barra-grafico barra-viendo"></div>
-      <div className="barra-grafico barra-pendiente"></div>
-      <div className="barra-grafico barra-terminada"></div>
-      <div className="barra-grafico barra-favorita"></div>
+      <Bar data={data} />
     </div>
   );
 };
